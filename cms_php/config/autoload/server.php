466326e510f777bae938a2a6e7ad30b378201cdb
yinmeng <>
2020-02-07 17:33:51 +0800

    bug

--- cms_php/config/autoload/server.php
+++ cms_php/config/autoload/server.php
@@ -21,7 +21,7 @@ return [
             'name' => 'http',
             'type' => Server::SERVER_HTTP,
             'host' => '0.0.0.0',
-            'port' => env('HTTP_PORT', 9501),
+            'port' => (int)env('HTTP_PORT', 9501),
             'sock_type' => SWOOLE_SOCK_TCP,
             'callbacks' => [
                 SwooleEvent::ON_REQUEST => [Hyperf\HttpServer\Server::class, 'onRequest'],
